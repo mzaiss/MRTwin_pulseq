b6634d7976256d6994eaeb01bb350b2ac3f745fc
arhiturbid <aloktyus@tuebingen.mpg.de>
2019-01-28 11:50:27 +0100

    fast gradopt allrep

--- codes/GradOpt/gradopt_allrep_pulseqtest.m
+++ codes/GradOpt/gradopt_allrep_pulseqtest.m
@@ -13,6 +13,8 @@ e = @(utrue,u) 100*norm(u(:)-utrue(:))/norm(utrue(:));
 
 %% Check numerical vs. analytical derivatives : optimize all reps simultaneously
 
+use_gpu = 0;
+
 T = 4;       % number of time points in readout                                                                                                  
 sz = [4, 6]; % image size (Nx Ny)          
 Nrep = 8;
@@ -39,7 +41,7 @@ use_tanh_grad_moms_cap = 1;                                                 % ot
 lambda = 0*1e-2;
 
 % pack the parameters for the gradient function
-args = {m,rampX,rampY,adc_mask,sz,Nrep,lambda,use_tanh_grad_moms_cap};
+args = {m,rampX,rampY,adc_mask,sz,Nrep,lambda,use_tanh_grad_moms_cap,use_gpu};
 [phi,dg_ana] = phi_grad_allrep_readout2d(g(:),args{:}); % compute loss and analytical derivatives
 
 % compute numerical derivatives
@@ -64,9 +66,11 @@ fprintf('deriv-err=%1.3f%%\n',e(dx_num,dg_ana(:)))
 %% optimize all reps simultaneously
 %close all;
 
+use_gpu = 1;
+
 % params
-NRep = 48;                                                                                                           % number of repetitions
-sz = [48,48];                                                                                                                   % image size
+NRep = 16;                                                                                                           % number of repetitions
+sz = [16,16];                                                                                                                   % image size
 T = sz(2);                                                                                        % set the number of time points in readout
 nmb_rand_restarts = 1;                                                                      % number of restarts with random initializations
 do_versbose = 0;                                                                         % additionally show learned Fourier basis functions
@@ -82,12 +86,12 @@ gtruth_m = imresize(gtruth_m,sz);  % resize to something managable
                                                                      
 % set the optimizer
 p = struct();
-nMVM = 10;  % number of optimization iterations
+nMVM = 100;  % number of optimization iterations
 p.length = -nMVM;
 p.method = 'LBFGS';
 
 % set ADC mask
-adc_mask = ones(T,1); adc_mask(1:4) = 0;
+adc_mask = ones(T,1); adc_mask(1:4) = 1;
 
 % set gradient spatial forms
 rampX = pi*linspace(-1,1,sz(1) + 1);
@@ -110,10 +114,15 @@ for rnd_restart = 1:nmb_rand_restarts
   %g = zeros(NRep,T,2); g(:,:,1) = rand(NRep,T,1) - 0.5; g = g(:);
   g = 1.0*(rand(NRep,T,2) - 0.5); g = g(:); 
   
-  p.length = -10;
+  p.length = -100;
 
   % do optimization for g of E(g), loss --> (||error_m - E.T*E*error_m||^2 + lambda*||cumsum(g)||^2) 
-  args = {gtruth_m,rampX,rampY,adc_mask,sz,NRep,lambda,use_tanh_grad_moms_cap};
+  if use_gpu
+    args = {gpuArray(single(gtruth_m)),gpuArray(single(rampX)),gpuArray(single(rampY)),gpuArray(single(adc_mask)),sz,NRep,lambda,use_tanh_grad_moms_cap,use_gpu};
+  else
+    args = {gtruth_m,rampX,rampY,adc_mask,sz,NRep,lambda,use_tanh_grad_moms_cap,use_gpu};
+  end  
+  
   [g,phi] = minimize(g(:),'phi_grad_allrep_readout2d',p,args{:});
 
   % select the gradients with the lowest loss achieved
@@ -136,14 +145,13 @@ grad_moms(:,:,2) = repmat(linspace(-sz(2)/2,sz(2)/2,sz(2)).',[1 sz(2),1]);
 
 %grad_moms = grad_moms + 0.2*randn(size(grad_moms));
 
-bestgrad = diff(cat(2,zeros(sz(1),1,2),grad_moms),[],2);
+bestgrad = diff(cat(2,zeros(sz(1),1,2),grad_moms),[],2); use_tanh_grad_moms_cap = 0;
 
 g = bestgrad;
 
 lambda = 0;
-use_tanh_grad_moms_cap = 0;
 
-args = {gtruth_m,rampX,rampY,adc_mask,sz,NRep,lambda,use_tanh_grad_moms_cap};
+args = {gtruth_m,rampX,rampY,adc_mask,sz,NRep,lambda,use_tanh_grad_moms_cap,0};
 [~,~,reco_current,E,grad_moms_output,grads] = phi_grad_allrep_readout2d(g(:),args{:});
 
 figure(3), hold off; plot(1);
