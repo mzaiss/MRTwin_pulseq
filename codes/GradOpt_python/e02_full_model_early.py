662232073ea7888c2d1dc2cbe6e32703cfa5b392
arhiturbid <aloktyus@tuebingen.mpg.de>
2019-01-31 14:55:28 +0100

    full model + opti

--- codes/GradOpt_python/e02_full_model_early.py
+++ codes/GradOpt_python/e02_full_model_early.py
@@ -11,12 +11,17 @@ import numpy as np
 import torch
 import cv2
 import matplotlib.pyplot as plt
+from torch import optim
+
+# NRMSE error function
+def e(gt,x):
+    return 100*np.linalg.norm((gt-x).ravel())/np.linalg.norm(gt.ravel())
 
 # %% full model early spec
 
 NRep = 10                                             # number of repetitions
 T = 8                                                # number of events F/R/P
-sz = [4,6]                                                       # image size
+sz = np.array([4,6])                                             # image size
 NSpins = 12
 
 Nvox = sz[0]*sz[1]
@@ -129,7 +134,7 @@ for t in range(T):
 def magimg(x):
   return np.sqrt(np.sum(np.abs(x)**2,2))
 
-sz = [16,16]                                                     # image size
+sz = np.array([16,16])                                           # image size
 NRep = sz[1]                                          # number of repetitions
 T = sz[0]                                            # number of events F/R/P
 NSpins = 2
@@ -165,7 +170,7 @@ adc_mask = np.ones((T,1) )
 F = torch.zeros((T,NRep,1,4,4), dtype=torch.float32)                         # FLIP pperator
 R = torch.zeros((Nvox,4,4), dtype=torch.float32)                             # RELAXATION operator
 G = torch.zeros((NRep,Nvox,4,4), dtype=torch.float32)                        # GRADIENT PRECESSION operator
-P = torch.zeros((NSpins,Nvox,4,4), dtype=torch.float32)                      # FREE PRECESSION operator: due to off-resonance component
+P = torch.zeros((NSpins,1,1,4,4), dtype=torch.float32)                       # FREE PRECESSION operator: due to off-resonance component
 
 # init tensors
 
@@ -225,68 +230,83 @@ B0_grad_sin = torch.sin(B0_grad)
 G[:,:,2,2] = 1
 G[:,:,3,3] = 1
  
-# free
-B0_nspins = dB0.view([NSpins,1])
+# free precession
+B0_nspins = dB0.view([NSpins])
 
 B0_nspins_cos = torch.cos(B0_nspins)
 B0_nspins_sin = torch.sin(B0_nspins)
  
-P[:,:,0,0] = B0_nspins_cos
-P[:,:,0,1] = -B0_nspins_sin
-P[:,:,1,0] = B0_nspins_sin
-P[:,:,1,1] = B0_nspins_cos
+P[:,0,0,0,0] = B0_nspins_cos
+P[:,0,0,0,1] = -B0_nspins_sin
+P[:,0,0,1,0] = B0_nspins_sin
+P[:,0,0,1,1] = B0_nspins_cos
 
 # Forward model  
-signal = torch.zeros((T,1,NRep,4,1), dtype=torch.float32) 
-signal[:,0,:,3,0] = 1
+signal = torch.zeros((T,NRep,4,1), dtype=torch.float32) 
+signal[:,:,3,0] = 1
 
-M0 = torch.zeros((Nvox,4), dtype=torch.float32)
-M0[:,2:] = 1
-M0[:,2] = M0[:,2] * PD.view([Nvox])
+M0 = torch.zeros((NSpins,NRep,Nvox,4), dtype=torch.float32)
+M0[:,:,:,2:] = 1
+M0[:,:,:,2] = M0[:,:,:,2] * PD.view([Nvox])
 
 # weight by proton density
 M_init = M0
 
 M = M_init.clone()
-M = M.view([Nvox,4,1])
+M = M.view([NSpins,NRep,Nvox,4,1])
 
 # flip
-M = torch.matmul(F[0,:,:,:],M)
+F[1:,:,0,0,0] = 1
+F[1:,:,0,0,2] = 0
+F[1:,:,0,2,0] = 0
+F[1:,:,0,2,2] = 1
 
 for t in range(T):
     
-    G[:,:,0,0] = B0_cos[t,:,:]
-    G[:,:,0,1] = -B0_sin[t,:,:]
-    G[:,:,1,0] = B0_sin[t,:,:]
-    G[:,:,1,1] = B0_cos[t,:,:]
+    # Flip
+    M = torch.matmul(F[t,:,:,:],M)
+    # Relax
+    M = torch.matmul(R,M)
+    
+    # gradient-driver precession
+    G[:,:,0,0] = B0_grad_cos[t,:,:]
+    G[:,:,0,1] = -B0_grad_sin[t,:,:]
+    G[:,:,1,0] = B0_grad_sin[t,:,:]
+    G[:,:,1,1] = B0_grad_cos[t,:,:]
     
     M = torch.matmul(G,M)
     
-    s = torch.sum(M,[0,2,4])
-    signal[t,0,:,:,0] = s
+    # free precession
+    M = torch.matmul(P,M)
     
+    # ADC -- read sig
+    s = torch.sum(M,[0,2,4])
+    signal[t,:,:,0] = s
+          
 B0X = torch.unsqueeze(grad_moms[:,:,0],2) * rampX_t
 B0Y = torch.unsqueeze(grad_moms[:,:,1],2) * rampY_t
 B0_grad = (B0X + B0Y).view([T,NRep,Nvox])
 
-B0 = B0_grad + B0_nspins
-B0_cos = torch.cos(B0)
-B0_sin = torch.sin(B0)
+B0_grad = B0_grad.permute([0,2,1])
 
-P[:,:,0,0] = B0_cos
-P[:,:,0,1] = -B0_sin
-P[:,:,1,0] = B0_sin
-P[:,:,1,1] = B0_cos
-          
-P = P.permute([0,1,3,2,5,4])
-P = P.view([T,Nvox,NRep,4,4])
+B0_grad_cos = torch.cos(B0_grad)
+B0_grad_sin = torch.sin(B0_grad)
+
+G = torch.zeros((Nvox,NRep,4,4), dtype=torch.float32)
 
 reco = torch.zeros((Nvox,2), dtype = torch.float32)
 
 for t in range(T-1,-1,-1):
-    r = torch.matmul(P[t,:,:,:,:],signal[t,:,:,:,:])
+    
+    G[:,:,0,0] = B0_grad_cos[t,:,:]
+    G[:,:,0,1] = B0_grad_sin[t,:,:]
+    G[:,:,1,0] = -B0_grad_sin[t,:,:]
+    G[:,:,1,1] = B0_grad_cos[t,:,:]    
+    
+    r = torch.matmul(G,signal[t,:,:,:])
     reco = reco + torch.sum(r[:,:,:2,0],1)
     
+target = reco.clone()
    
 reco = reco.cpu()
 reco = reco.numpy().reshape([sz[0],sz[1],2])
@@ -298,4 +318,168 @@ plt.show()
 
 plt.imshow(magimg(reco))
 plt.title('reconstruction')
+plt.ion()
+plt.show()
+
+
+# %% optimize
+
+def phi_FRP_model(grads,args):
+    
+    target,M_init,use_tanh_grad_moms_cap = args
+    
+    M = M_init.clone()
+    M = M.view([NSpins,NRep,Nvox,4,1])
+    
+    # gradients
+    rampX_t = torch.from_numpy(rampX).float()
+    rampX_t = rampX_t.view([1,1,Nvox])
+    
+    rampY_t = torch.from_numpy(rampY).float()
+    rampY_t = rampY_t.view([1,1,Nvox])
+    
+    grad_moms = torch.cumsum(grads,0)
+    
+    if use_tanh_grad_moms_cap:
+      boost_fct = 1
+        
+      fmax = sz / 2
+      for i in [0,1]:
+          grad_moms[:,:,i] = boost_fct*fmax[i]*torch.tanh(grad_moms[:,:,i])
+          
+    temp = torch.cat((torch.zeros((1,NRep,2),dtype=torch.float32),grad_moms),0)
+    grads_cap = temp[1:,:,:] - temp[:-1,:,:]
+    
+    B0X = torch.unsqueeze(grads_cap[:,:,0],2) * rampX_t
+    B0Y = torch.unsqueeze(grads_cap[:,:,1],2) * rampY_t
+    
+    B0_grad = (B0X + B0Y).view([T,NRep,Nvox])
+    
+    B0_grad_cos = torch.cos(B0_grad)
+    B0_grad_sin = torch.sin(B0_grad)    
+    
+    G = torch.zeros((NRep,Nvox,4,4), dtype=torch.float32)
+    G[:,:,2,2] = 1
+    G[:,:,3,3] = 1   
+     
+    # init signal 
+    signal = torch.zeros((T,NRep,4,1), dtype=torch.float32) 
+    signal[:,:,3,0] = 1
+    
+    for t in range(T):
+        
+        # Flip
+        M = torch.matmul(F[t,:,:,:],M)
+        # Relax
+        M = torch.matmul(R,M)
+        
+        # gradient-driver precession
+        G[:,:,0,0] = B0_grad_cos[t,:,:]
+        G[:,:,0,1] = -B0_grad_sin[t,:,:]
+        G[:,:,1,0] = B0_grad_sin[t,:,:]
+        G[:,:,1,1] = B0_grad_cos[t,:,:]
+        
+        M = torch.matmul(G,M)
+        
+        # free precession
+        M = torch.matmul(P,M)
+        
+        # ADC -- read sig
+        s = torch.sum(M,[0,2,4])
+        signal[t,:,:,0] = s
+              
+    B0X = torch.unsqueeze(grad_moms[:,:,0],2) * rampX_t
+    B0Y = torch.unsqueeze(grad_moms[:,:,1],2) * rampY_t
+    B0_grad = (B0X + B0Y).view([T,NRep,Nvox])
+    
+    B0_grad = B0_grad.permute([0,2,1])
+    
+    B0_grad_cos = torch.cos(B0_grad)
+    B0_grad_sin = torch.sin(B0_grad)
+    
+    #G = torch.zeros((Nvox,NRep,4,4), dtype=torch.float32)
+    G = G.permute([1,0,2,3])
+    G[:,:,2,2] = 1
+    G[:,:,3,3] = 1    
+    
+    reco = torch.zeros((Nvox,2), dtype = torch.float32)
+    
+    for t in range(T-1,-1,-1):
+        
+        G[:,:,0,0] = B0_grad_cos[t,:,:]
+        G[:,:,0,1] = B0_grad_sin[t,:,:]
+        G[:,:,1,0] = -B0_grad_sin[t,:,:]
+        G[:,:,1,1] = B0_grad_cos[t,:,:]    
+        
+        r = torch.matmul(G,signal[t,:,:,:])
+        reco = reco + torch.sum(r[:,:,:2,0],1)
+        
+    loss = (reco - target)
+    phi = torch.sum((1.0/Nvox)*torch.abs(loss.squeeze())**2)
+    
+    return (phi,reco)
+
+M0 = torch.zeros((NSpins,NRep,Nvox,4), dtype=torch.float32)
+M0[:,:,:,2:] = 1
+M0[:,:,:,2] = M0[:,:,:,2] * PD.view([Nvox])
+
+# weight by proton density
+M_init = M0.clone()
+
+# init gradients
+
+g = np.random.rand(T,NRep,2) - 0.5
+#g = g.ravel()
+grads = torch.from_numpy(g).float()
+grads.requires_grad = True
+
+target_numpy = target.detach()
+target_numpy = target_numpy.cpu()
+target_numpy = target_numpy.numpy().reshape([sz[0],sz[1],2])
+
+use_tanh_grad_moms_cap = 1
+
+args = (target,M_init,use_tanh_grad_moms_cap)
+
+optimizer = optim.LBFGS([grads], max_iter=1,history_size=200)
+
+def weak_closure():
+    optimizer.zero_grad()
+    loss,_ = phi_FRP_model(grads, args)
+    
+    loss.backward()
+    
+    return loss
+
+training_iter = 50
+for i in range(training_iter):
+    optimizer.step(weak_closure)
+    
+    _,reco = phi_FRP_model(grads, args)
+    
+    reco = reco.detach()
+    reco = reco.cpu()
+    reco = reco.numpy().reshape([sz[0],sz[1],2])
+    
+    print("error =%f" %e(target_numpy.ravel(),reco.ravel()))
+    
+
+phi,reco = phi_FRP_model(grads, args)
+
+reco = reco.detach()
+reco = reco.cpu()
+reco = reco.numpy().reshape([sz[0],sz[1],2])
+
+plt.imshow(magimg(reco))
+plt.title('reconstruction')
+
+
+
+
+
+
+
+
+
+
 
