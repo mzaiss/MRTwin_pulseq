e33fcbc6b1fe2187a7a0d358d10ceeae1fc8cdde
fglang <43142740+fglang@users.noreply.github.com>
2019-03-12 18:31:51 +0100

    added simple FFT reconstruction for measured raw data

---
+++ codes/ScannerReco/RECO_pulseq_simpleFFT.m
@@ -0,0 +1,54 @@
+%% very simple FFT reconstruction from raw data
+
+%% Load raw data
+[datfilename,datpath] = uigetfile('*.dat', 'select raw data file');
+data_file_path=[datpath datfilename];
+
+twix_obj = mapVBVD(data_file_path);
+
+
+%% sort in the k-space data
+if iscell(twix_obj)
+    data_unsorted = twix_obj{2}.image.unsorted();
+else
+    data_unsorted = twix_obj.image.unsorted();
+end
+
+% the incoming data order is [kx coils acquisitions]
+data_coils_last = permute(data_unsorted, [1, 3, 2]);
+nCoils = size(data_coils_last, 3);
+
+data = data_coils_last;
+
+%% show kspace data
+figure(101)
+subplot(2,2,1), imagesc1t(rot90(abs(data(:,:,1)))), title('coil 1'), axis('image')
+subplot(2,2,2), imagesc1t(rot90(abs(data(:,:,2)))), title('coil 2'), axis('image')
+
+%% Reconstruct coil images
+
+images = zeros(size(data));
+%figure;
+
+for ii = 1:nCoils
+    %images(:,:,:,ii) = fliplr(rot90(fftshift(fft2(fftshift(data(:,:,:,ii))))));
+    images(:,:,ii) = fftshift(fft2(fftshift(data(end:-1:1,:,ii))));
+    %subplot(2,2,ii);
+    %imshow(abs(images(:,:,ii)), []);
+    %title(['RF Coil ' num2str(ii)]);
+    %for ni = 1:nImages
+        %tmp = abs(images(:,:,ni,ii));
+        %tmp = tmp./max(tmp(:));
+        %imwrite(tmp, ['img_coil_' num2str(ii) '.png'])
+    %end
+end
+
+% Phase images (possibly channel-by-channel and echo-by-echo)
+% figure;
+% imab(angle(images));colormap('jet');
+
+%% Sum of squares combination of channels
+sos=abs(sum(images.^2,ndims(images)).^(1/2));
+sos=sos./max(sos(:));
+figure(101), subplot(2,2,3)
+imagesc1t(rot90(sos)), title('reco coilcombination'), axis('image')
