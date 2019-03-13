fixed RARE with grad spoiler fixed pulseconverter for gradients in first action plotting in traget_seq fixed for FA

codes/GradOpt_python/core/target_seq_holder.py
@@ -53,7 +53,7 @@ class TargetSequenceHolder():
             except:
                 FA=self.flips
                 
-            ax=plt.imshow(tonumpy(FA*180/np.pi),cmap=plt.get_cmap('nipy_spectral'))
+            ax=plt.imshow(np.transpose(tonumpy(FA*180/np.pi),[1,0]),cmap=plt.get_cmap('nipy_spectral'))
             plt.ion()
             plt.title('FA [\N{DEGREE SIGN}]')
             plt.clim(-90,270)

codes/GradOpt_python/e15_2D_RARE_SARCOST_adjointzoo.py
@@ -180,9 +180,11 @@ else:
     grad_moms[1,:,1] = torch.linspace(-int(sz[1]/2),int(sz[1]/2-1),int(NRep))
     grad_moms[-1,:,1] = -torch.linspace(-int(sz[1]/2),int(sz[1]/2-1),int(NRep))
         
-grad_moms[0,0,0] = -torch.ones((1,1))*sz[0]/2  # RARE: rewinder after 90 degree half length, half gradmom
+grad_moms[0,0,0] =grad_moms[0,0,0]- torch.ones((1,1))*sz[0]/2  # RARE: rewinder after 90 degree half length, half gradmom
+
 grad_moms[1,:,0] =  torch.ones((1,1))*sz[0]  # RARE: rewinder after 90 degree half length, half gradmom
 grad_moms[-1,:,0] =  torch.ones((1,1))*sz[0]  # RARE: rewinder after 90 degree half length, half gradmom
+
 
 # dont optimize y  grads
 #grad_moms[:,:,1] = 0
@@ -288,7 +290,7 @@ def phi_FRP_model(opt_params,aux_params):
     scanner.forward(spins, event_time)
     scanner.adjoint(spins)
 
-    lbd = 1e3
+    lbd = 1e1
             
     loss_image = (scanner.reco - targetSeq.target_image)
     #loss_image = (magimg_torch(scanner.reco) - magimg_torch(targetSeq.target_image))
@@ -425,7 +427,7 @@ for i in range(3):
     plt.show()
     
 # %% # save optimized parameter history
-experiment_id = 'RARE_FA_OPT_fixrep1_90_adjflipgrad'
+experiment_id = 'RARE_FA_OPT_fixrep1_90_adjflipgrad_spoiled'
 #opt.save_param_reco_history(experiment_id)
 
 opt.scanner_opt_params[0][0,0,:] = 90*np.pi/180