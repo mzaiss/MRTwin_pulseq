bffdd5c4734f936fb0e7b5c6ef3ad61a86c9bb8f
mzaiss <altustro@gmail.com>
2019-04-24 12:01:32 +0200

    added some stuff for bssfp reco and RARE reco

--- codes/GradOpt_python/matlab/ScannerReco/multiRECO_pulseq_simpleFFT.m
+++ codes/GradOpt_python/matlab/ScannerReco/multiRECO_pulseq_simpleFFT.m
@@ -3,7 +3,7 @@ function multiRECO_pulseq_simpleFFT
 addpath('D:\root\ZAISS_LABLOG\LOG_MPI\0_CESTtool\CEST_EVAL_GLINT\reco_raw\recoVBVD')
 addpath('D:\root\ZAISS_LABLOG\LOG_MPI\0_CESTtool\CEST_EVAL_GLINT\reco_raw\mapVBVD')
 %% Load raw data
-[filename,rawpname] = uigetfile({'*.dat'},['select raw fils for reconstruction'],'MultiSelect','on','\\mrz3t\Upload\CEST_seq\MRIZERO_data');
+[filename,rawpname] = uigetfile({'*.dat'},['select raw fils for reconstruction'],'MultiSelect','on','\\mrz3t\Upload\CEST_seq\pulseq_zero\sequences');
 if iscell(filename)==0
     temp=filename; clear filename;
     filename{1}=temp; clear temp;
@@ -18,11 +18,21 @@ array=1:numel(filename);
 twix_obj = mapVBVD([rawpname '/' filename{1}]);
 [sos_base, phase_base] = TWIXtoIMG(twix_obj);
 
+seq=mr.Sequence();
+seq.read([rawpname '/' sprintf('pulseq.seq')]);
+[ktraj_adc, ktraj] = seq.calculateKspace();
+
+%MEAS
+
+
 for ii=array
     
-twix_obj = mapVBVD([rawpname '/' filename{ii}]);
-[sos, phase] = TWIXtoIMG(twix_obj);
+%twix_obj = mapVBVD([rawpname '/' filename{ii}]);
+%[sos, phase] = TWIXtoIMG(twix_obj);
 
+twix_obj=mapVBVD([rawpname '/' filename{ii}]);
+[sos, phase] = TWIXtoIMG_ADJOINT_gradmoms_from_pulseq(twix_obj,ktraj_adc);
+figure(1),
 subplot(2,2,1), imagesc(rot90(sos),[0 1]), title(sprintf('reco sos, iter %d',ii)), axis('image'); colorbar;
 subplot(2,2,3), imagesc(rot90(phase)), title('reco phase coil(1) '), axis('image'); colorbar;
 subplot(2,2,2), imagesc(rot90(sos_base),[0 1]), title(sprintf('MEAS reco sos, iter %d',1)), axis('image'); colorbar;
@@ -30,22 +40,22 @@ subplot(2,2,4), imagesc(rot90(phase_base)), title('reco phase coil(1) '), axis('
 set(gcf, 'Outerposition',[404   356   850   592])
 
 % create gif (out.gif)
-drawnow
-      frame = getframe(1);
-      im = frame2im(frame);
-      im_MEAS(:,:,:,ii)=im;
-      [imind,cm] = rgb2ind(im,32);
-      if ii == 1
-          imwrite(imind,cm,'out.gif','gif', 'Loopcount',inf);
-      elseif ii==numel(array)
-          imwrite(imind,cm,'out.gif','gif','WriteMode','append','DelayTime',3);
-      else
-          imwrite(imind,cm,'out.gif','gif','WriteMode','append','DelayTime',0.0005);
-      end
+% drawnow
+%       frame = getframe(1);
+%       im = frame2im(frame);
+%       im_MEAS(:,:,:,ii)=im;
+%       [imind,cm] = rgb2ind(im,32);
+%       if ii == 1
+%           imwrite(imind,cm,'out.gif','gif', 'Loopcount',inf);
+%       elseif ii==numel(array)
+%           imwrite(imind,cm,'out.gif','gif','WriteMode','append','DelayTime',3);
+%       else
+%           imwrite(imind,cm,'out.gif','gif','WriteMode','append','DelayTime',0.0005);
+%       end
 end
 set(0, 'DefaultLineLineWidth', 0.5);
 last_fn_meas=filename{ii};
-save('togif.mat','im_MEAS','last_fn_meas');
+% save('togif.mat','im_MEAS','last_fn_meas');
 end
 
 
@@ -79,15 +89,20 @@ sos=abs(sum(images.^2,ndims(images)).^(1/2));
 SOS=sos./max(sos(:));
 phase = angle(images(:,:,ii));
 
-if 0
+%% reconstruct using adjoint
+    
+
+
+%%
+if 1
 %% Sum of squares combination of channels
 figure(101), subplot(2,2,3)
 imagesc(rot90(sos)), title('reco coilcombination'), axis('image')
 
-show kspace data
+% show kspace data
 figure(101)
-subplot(2,2,1), imagesc(rot90(abs(data(:,:,1)))), title('coil 1'), axis('image')
-subplot(2,2,2), imagesc(rot90(abs(data(:,:,2)))), title('coil 2'), axis('image')
+subplot(2,2,1), imagesc(rot90(abs(data(:,:,1)))), title('kspace coil 1'), axis('image')
+subplot(2,2,2), imagesc(rot90(abs(data(:,:,2)))), title('kspace coil 2'), axis('image')
 end
 
 end
