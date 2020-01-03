643abe221bec84b63c2e814389e507e6b471f2fa
mzaiss <altustro@gmail.com>
2020-01-03 19:45:43 +0100

    added A03 and A04

--- code/MRzero/help.py
+++ code/MRzero/help.py
@@ -9,3 +9,6 @@ Created on Fri Jan  3 12:02:42 2020
 #https://pyformat.info/
 "f={:.2}*exp(-{:.2}*t)+{}".format(5.0, 7.1,'constant')
 
+
+TypeError: can't assign a numpy.ndarray to a torch.FloatTensor
+solution: torch.from_numpy(Trec)
