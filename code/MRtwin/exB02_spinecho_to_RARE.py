79da611cb0d5773cb262472b44ea780c6c694d00
mzaiss <moritz.zaiss@uk-erlangen.de>
2022-03-06 11:57:53 +0100

    reset to state without sol

--- code/MRtwin/exB02_spinecho_to_RARE.py
+++ code/MRtwin/exB02_spinecho_to_RARE.py
@@ -84,7 +84,7 @@ def setdevice(x):
 
 #############################################################################
 ## S0: define image and simulation settings::: #####################################
-sz = np.array([24,24])                      # image size
+sz = np.array([32,32])                      # image size
 extraMeas = 1                               # number of measurmenets/ separate scans
 NRep = extraMeas*sz[1]                      # number of total repetitions
 szread=sz[0]
@@ -144,12 +144,12 @@ scanner.set_adc_mask(adc_mask=setdevice(adc_mask))
 
 # RF events: rf_event and phases
 rf_event = torch.zeros((NEvnt,NRep,4), dtype=torch.float32)
-rf_event[1,0,0] = 90*np.pi/180  # GRE/FID specific, GRE preparation part 1 : 90 degree excitation 
-rf_event[1,0,1] = 90*np.pi/180  # GRE/FID specific, GRE preparation part 1 : 90 degree excitation 
-rf_event[3,:,0] = 180*np.pi/180  # GRE/FID specific, GRE preparation part 1 : 90 degree excitation 
-rf_event[3,:,3] = 2  # is refocpulse
-rf_event[1,0,0] = 1  # is expulse
-  
+rf_event[1,:,0] = 90*np.pi/180 
+rf_event[1,:,1] = 90*np.pi/180  
+rf_event[1,:,3] = 1  # is expulse
+rf_event[3,:,0] = 180*np.pi/180  
+rf_event[3,:,3] = 2    # is refocpulse
+
 rf_event = setdevice(rf_event)
 scanner.init_flip_tensor_holder()    
 scanner.set_flip_tensor_withB1plus(rf_event)
@@ -158,28 +158,29 @@ rfsign = ((rf_event[3,:,0]) < 0).float()
 scanner.set_ADC_rot_tensor(-rf_event[3,0,1] + np.pi/2 + np.pi*rfsign) #GRE/FID specific
 
 # event timing vector 
-event_time = torch.from_numpy(0.08*1e-3*np.ones((NEvnt,NRep))).float()
-#event_time[4,:] =0.01
-TE=torch.sum(event_time[3:,0])
-event_time[2,:] =  (TE-torch.sum(event_time[1:3,0]))/2
-event_time[2,1:] =  0
-event_time[-1,:] =  0
+event_time = torch.from_numpy(0.08*8*1e-3*np.ones((NEvnt,NRep))).float()
+
+idx_echo= int(5 + (NEvnt-7)/2)
+TE2_2 = event_time[3:idx_echo,0].sum()
+
+event_time[2,:] = TE2_2 - 0.08*1e-3*2
+TE2_1 = event_time[1:3,0].sum() 
+
+event_time[-1,:] =  5
 event_time = setdevice(event_time)
+
+
+TA = tonumpy(torch.sum(event_time))
+TR = tonumpy(torch.sum(event_time[:,0]))
+TE = TE2_1 + TE2_2
+
 
 # gradient-driver precession
 # Cartesian encoding
 gradm_event = torch.zeros((NEvnt,NRep,2), dtype=torch.float32)
-gradm_event[2,0,1] = 0.5*szread + szread*0.5   # read
-
-gradm_event[4,:,0] = torch.linspace(-int(sz[1]/2),int(sz[1]/2-1),int(NRep))  # phase encoding blip in second event block
-gradm_event[4,:,1] = szread*0.5   # spoiler
-
-gradm_event[5:-2,:,1] = 1 # read
-
-gradm_event[-2,:,0] = -gradm_event[4,:,0]   #rewind phase
-gradm_event[-2,:,1] = szread*0.5   # spoiler
-
-
+gradm_event[2,:,1] = 0.5*szread
+gradm_event[5:-2,:,1] = 1
+gradm_event[2,:,0] = torch.linspace(-int(sz[1]/2),int(sz[1]/2-1),int(NRep))  # phase encoding blip in second event block
 gradm_event = setdevice(gradm_event)
 
 scanner.init_gradient_tensor_holder()
@@ -190,19 +191,19 @@ scanner.set_gradient_precession_tensor_super(gradm_event,rf_event)  # refocusing
 #############################################################################
 ## S4: MR simulation forward process ::: #####################################
 scanner.init_signal()
-scanner.forward(spins, event_time)
+scanner.forward_fast(spins, event_time)
   
 
 targetSeq = core.target_seq_holder.TargetSequenceHolder(rf_event,event_time,gradm_event,scanner,spins,scanner.signal)
 targetSeq.print_seq_pic(True,plotsize=[12,9])
-targetSeq.print_seq(plotsize=[12,9], time_axis=1)
+targetSeq.print_seq(plotsize=[12,9],time_axis=1)     
   
 #%% ############################################################################
 ## S5: MR reconstruction of signal ::: #####################################
 
 spectrum = tonumpy(scanner.signal[0,adc_mask.flatten()!=0,:,:2,0].clone()) 
 spectrum = spectrum[:,:,0]+spectrum[:,:,1]*1j # get all ADC signals as complex numpy array
-spectrum_adc=spectrum
+kspace = spectrum
 
 space = np.zeros_like(spectrum)
 spectrum = np.roll(spectrum,szread//2,axis=0)
@@ -217,7 +218,7 @@ plt.imshow(phantom[:,:,0].transpose(), interpolation='none'); plt.xlabel('PD')
 plt.subplot(4,6,20)
 plt.imshow(phantom[:,:,3].transpose(), interpolation='none'); plt.xlabel('dB0')
 plt.subplot(4,6,22)
-plt.imshow(np.abs(spectrum_adc).transpose(), interpolation='none'); plt.xlabel('kspace')
+plt.imshow(np.abs(kspace).transpose(), interpolation='none'); plt.xlabel('kspace')
 plt.subplot(4,6,23)
 plt.imshow(np.abs(space).transpose(), interpolation='none',aspect = sz[0]/szread); plt.xlabel('mag_img')
 plt.subplot(4,6,24)
