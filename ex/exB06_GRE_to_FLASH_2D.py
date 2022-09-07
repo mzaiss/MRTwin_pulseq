b37c5ff76755e3752adc91b57657085505ad876a
mzaiss <moritz.zaiss@tuebingen.mpg.de>
2022-09-07 08:14:43 +0200

    added new ex and old solutions

---
+++ ex/exB06_GRE_to_FLASH_2D.py
@@ -0,0 +1,3 @@
+experiment_id = 'exB06_GRE_to_FLASH_2D'
+
+# copy everything except the experimeht ID for solB05_clean_GRE_2D_fully_relaxed
---
+++ ex/exB09_GRE_EPI_2D.py
@@ -0,0 +1,3 @@
+experiment_id = 'exB09_GRE_EPI_2D'
+
+# copy everything except the experimeht ID for solB05_clean_GRE_2D_fully_relaxed
