42c0bd3e9425867cea03d007d825ec17f662ab43
J-Endres <jonathan.endres@uk-erlangen.de>
2022-11-29 14:33:37 +0100

    Removed obsolete SimData

--- codes/GradOpt_python/new_core/sim_data/__init__.py
+++ codes/GradOpt_python/new_core/sim_data/__init__.py
@@ -1,5 +1,4 @@
 # Backwards compatibility, contains the old SimData
-from .old_sim_data import *
 from .voxel_grid_phantom import VoxelGridPhantom
 from .raw_sim_data import RawSimData
 from .custom_voxel_phantom import CustomVoxelPhantom
