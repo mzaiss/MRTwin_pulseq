03df7f3db5c1cfe113a7d051c2ff0b7fe171e83c
J-Endres <jonathan.endres@uk-erlangen.de>
2022-11-29 13:28:03 +0100

    bugfix: no quantification phantom for MRITwin_pulseq

--- codes/GradOpt_python/new_core/sim_data/__init__.py
+++ codes/GradOpt_python/new_core/sim_data/__init__.py
@@ -3,4 +3,3 @@ from .old_sim_data import *
 from .voxel_grid_phantom import VoxelGridPhantom
 from .raw_sim_data import RawSimData
 from .custom_voxel_phantom import CustomVoxelPhantom
-from .quantification_phantom import QuantificationPhantom
