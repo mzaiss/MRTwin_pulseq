2c76ff5c4f6b1d95181037abf77736a1ae726bd5
mzaiss <moritz.zaiss@tuebingen.mpg.de>
2023-02-12 10:35:20 +0100

    added a simpler centric reordering and inverse permutation

--- ex/ex_help04_permute.py
+++ ex/ex_help04_permute.py
@@ -39,3 +39,19 @@ inverse_perm = np.arange(len(permvec))[np.argsort(permvec)]
 
 CC=BB[:,inverse_perm]
 
+#%% ############################################################################
+# simpler centric permutation
+import numpy as np
+lst=np.linspace(1,9,10)
+
+permutation = sorted(np.arange(len(lst)), key=lambda x: abs(len(lst)//2 - x))
+sorted_lst = lst[permutation]
+
+# Apply the same permutation to another array
+other_arr = np.linspace(1,5,10)
+sorted_other_arr = other_arr[permutation]
+
+# Invert the permutation to restore the original order
+inverted_permutation = np.argsort(permutation)
+restored_lst = sorted_lst[inverted_permutation]
+restored_other_arr = sorted_other_arr[inverted_permutation]
