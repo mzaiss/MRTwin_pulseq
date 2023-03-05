578d634a65fda00ef3f42d1ffedc18ba2811b2e8
Jonathan <jonathan.endres01@gmail.com>
2023-03-05 12:38:16 +0100

    updeated rest of B exercises

--- ex/exB07_FLASH_2D_reordering.py
+++ ex/exB07_FLASH_2D_reordering.py
@@ -1,6 +1,4 @@
 experiment_id = 'exB07_FLASH_2D_reordering'
 
-
 # copy everything except the experimeht ID for solB05_clean_GRE_2D_fully_relaxed
-#helpful file: ex_help04
-
+# helpful file: ex_help04
