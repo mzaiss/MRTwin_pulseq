3db331f3faf2fd7ee17b8e4dbaa76cde1252c914
J-Endres <67545816+J-Endres@users.noreply.github.com>
2023-03-09 18:04:06 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone -b mr0-core https://github.com/mzaiss/MRTwin_pulseq.git
-
-module load python3/anaconda-2022.05
-pip install MRTwin_pulseq/data/MRzeroCore-0.1.0-cp39-cp39-manylinux_2_28_x86_64.whl --force-reinstall
-pip install pypulseq
