4ccf84678dc9bfbd59ec6db034c852f7c2d41ab7
mzaiss <mzaiss@users.noreply.github.com>
2023-09-01 11:02:22 +0200

    Update exZ01_.py

--- ex/info/exZ01_.py
+++ ex/info/exZ01_.py
@@ -1,74 +1,84 @@
 '''
 Exercises - Homework:
 
-Z01.1. Spin echo EPI  with zig-zag or non blipped trajectory
+1.1. Spin echo EPI  with zig-zag or non blipped trajectory and silent EPI   (evtl propeller EPI)
 hint: exersice or any textbook for spin echo, http://mri.beckman.illinois.edu/interactive/topics/contents/fast_imaging/figures/zig.shtml
+hint: Fig 2 in https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5836719/, reuse radial reco
  reuse radial reco
 
-Z01.2. DREAM MRI
+
+1.2. DREAM MRI
 two readouts from one measurement: eg. FID, STE as two separate contrasts
 hint: https://onlinelibrary.wiley.com/doi/full/10.1002/mrm.24158
 
-Z01.3. Diffusion weighted MRI
+
+1.3. Diffusion weighted MRI
 GRE DWI and spin echo DWI
 hint: any textbook, https://blog.ismrm.org/2017/06/06/dwe-part-2/ 
 
 
-Z01.4. B0 mapping  
-hint: FLASH phase maps, any text book
+1.4. B0 mapping  
+hint: FLASH phase maps, any text book, both TE after one excitation
 
-Z01.5. B1 mapping  
+
+1.5. B1 mapping  
 hint: Double angle https://onlinelibrary.wiley.com/doi/10.1002/mrm.20896 or (cosine fit)
 reuse MP-FLASH
 
-Z01.6a. T1 mapping  (invrec)
+
+1.6a. T1 mapping  (invrec)
 hint: any textbook, reuse MP-LASH
 
-Z01.6a. T1 mapping  (two flipangles)
+
+1.6a. T1 mapping  (two flipangles)
 hint: equation 3a and 3b of https://onlinelibrary.wiley.com/doi/full/10.1002/mrm.21969
 
-Z01.7a. T2 mapping (Spin echo)
+1.7a. T2 mapping (Spin echo)
 hint: any textbook, reuse SE or RARE
 
-Z01.7a. T2 mapping (t2prep)
+1.7a. T2 mapping (t2prep)
 hint: https://www.kjronline.org/ViewImage.php?Type=F&aid=549810&id=F6&afn=68_KJR_18_1_113&fn=kjr-18-113-g006_0068KJR
 reuse FLASH
 
-Z01.8. FISP / PSIF 
+1.8. FISP / PSIF 
 hint: https://mriquestions.com/psif-vs-fisp.html
 oder (fat suppression / water fat imaging)
 
-
-Z01.9. HASTE sequence 
+1.9. HASTE sequence  + POCS recon
 hint: textbook and https://mriquestions.com/partial-fourier.html
 
-
-Z01.10. Spiral Trajectory -  EPI
+1.10. Spiral Trajectory -  EPI
 any textbook and https://pulseq.github.io/, reuse radial reco
 
-Z01.11  silent EPI -   cosine 
-hint: Fig 2 in https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5836719/, reuse radial reco
+1.11  eight ball echo
 
-Z01.12:  compressed sensing radial MRI
-hint: exercise F01 F02, Studon Hoppe-Script, 
+1.12  full free FOV rotation translation
+
+1.13   Segmentation  - segmented high res MPRAGE - segmented high res TSE
+
+1.14   BURST MRI (advanced)
+
+1.15 Ernst angle derivation - application to FID and FLASH
+
+1.16 B0 distortion correction in EPI
+
+1.17 Fastest Flash with trapezoidal gradients and ramp sampling
 
 
-Z02.1. Display all Spins as a vector or many vectors
 
-Z02.2. Display all Spins as a vector or many vectors, and animate this during a sequence
 
-Z02.3. Ernst angle derivation - application to FID and FLASH
+2.1. Code a small Bloch simulator an visualize spins
 
-Z02.4. B0 distortion correction in EPI
 
 
 
 """
 
-Tuesday to Thirsday 
+Tuesday to Thursday 
 
 17:00 Zoom meeting for questions
 
 Excursion Henkestrasse 91 ALDI other side of the street
+
 
  
