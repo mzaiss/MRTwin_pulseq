e863e08e72bd4739bf6825d881019b861f4dbaae
mzaiss <mzaiss@users.noreply.github.com>
2024-02-27 14:24:48 +0100

    Update exB02_gradient_echo_freq_enc.py

--- ex/exB02_gradient_echo_freq_enc.py
+++ ex/exB02_gradient_echo_freq_enc.py
@@ -173,6 +173,7 @@ plt.title('phantom projection')
 import torch.nn.functional as F
 phantom_resampled = F.interpolate(PD.permute(2, 0, 1).unsqueeze(0) , size=(Nread, Nread), mode='nearest').squeeze(0)  # Squeeze to remove batch dimension if it was added
 phantom_resampled = np.roll(phantom_resampled, -Nread // sz[1] // 2 + 1, 0)  # this is needed due to the oversampling of the phantom, szread>sz
+phantom_resampled=phantom_resampled.squeeze()
 
 if gx.channel == 'x':
     plt.plot(np.sum(phantom_resampled, axis=1).flatten('F'), label='y-projection')
