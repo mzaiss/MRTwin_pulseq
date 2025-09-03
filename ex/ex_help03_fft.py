95d17ce9f8dffa32fe514e0d6cb1bab6e1f33ac8
mzaiss <mzaiss@users.noreply.github.com>
2025-09-03 11:35:45 +0200

    switched to torch to match exercises

--- ex/ex_help03_fft.py
+++ ex/ex_help03_fft.py
@@ -1,57 +1,57 @@
-from scipy import signal
-import numpy as np
+import scipy
+import torch 
 import matplotlib.pyplot as plt
+case="gauss"
+if case=="block":
+    samplepoints = 112
+    func = torch.zeros((samplepoints,),dtype=torch.complex128)
+    func[20:70] = 1+0.1j
+elif case=="gauss":
+    func = scipy.signal.gaussian(samplepoints, std=5)*(1+0.1j)
 
-samplepoints = 112
-func = np.zeros((samplepoints,),dtype=np.complex128)
-func[20:70] = 1+0.1j
+func = torch.tensor(func,dtype=torch.complex128)
 
-# func = signal.gaussian(samplepoints, std=5)
-# func = (np.sin(0.1 * np.arange(1, samplepoints, 1)) +
-#         0.25 * np.sin(0.5 * np.arange(1, samplepoints, 1)))
-# func = np.roll(func, samplepoints // 2 + 11, axis=0)
-
-FFT_func = np.fft.ifft(func)
-FFT_FFT_func = np.fft.fft(FFT_func)
+FFT_func = torch.fft.ifft(func)
+FFT_FFT_func = torch.fft.fft(FFT_func)
 
 plt.subplot(321)
 plt.title('func')
-plt.plot(np.real(func))
-plt.plot(np.imag(func)); plt.legend(['real','imag'])
+plt.plot(torch.real(func))
+plt.plot(torch.imag(func)); plt.legend(['real','imag'])
 
 plt.subplot(323)
 plt.title('ifft(func)')
-plt.plot(np.real(FFT_func))
-plt.plot(np.imag(FFT_func)); plt.legend(['real','imag'])
+plt.plot(torch.real(FFT_func))
+plt.plot(torch.imag(FFT_func)); plt.legend(['real','imag'])
 plt.subplot(325)
 plt.title('fft(ifft(func))')
-plt.plot(np.abs(FFT_FFT_func))
-plt.plot(np.imag(FFT_FFT_func)); plt.legend(['real','imag'])
+plt.plot(torch.abs(FFT_FFT_func))
+plt.plot(torch.imag(FFT_FFT_func)); plt.legend(['real','imag'])
 
 
 
 ## echo like function
-func_echolike = np.fft.fftshift(FFT_func, 0) #an echo is the shifted ifft of an object
+func_echolike = torch.fft.fftshift(FFT_func, axis=0) #an echo is the shifted ifft of an object
 
 plt.subplot(322)
 plt.title('echo_func \n(an echo is the shifted ifft of an object) ')
-plt.plot(np.abs(func_echolike))
-plt.plot(np.imag(func_echolike)); plt.legend(['real','imag'])
+plt.plot(torch.abs(func_echolike))
+plt.plot(torch.imag(func_echolike)); plt.legend(['real','imag'])
 
 
 # fft of wrongly fft-shifted
-FFT_func_echolike = np.fft.fft(func_echolike)
+FFT_func_echolike = torch.fft.fft(func_echolike, axis=0)
 
 plt.subplot(324)
 plt.title('FFT(echo)')
-plt.plot(np.abs(FFT_func_echolike))
-plt.plot(np.imag(FFT_func_echolike)); plt.legend(['real','imag'])
+plt.plot(torch.abs(FFT_func_echolike))
+plt.plot(torch.imag(FFT_func_echolike)); plt.legend(['real','imag'])
 
 # fft of rolled fft-shifted
 
-FFT_func_echolike_shifted = np.fft.fft(np.fft.ifftshift(func_echolike, 0))
+FFT_func_echolike_shifted = torch.fft.fft(torch.fft.ifftshift(func_echolike, axis=0))
 
 plt.subplot(326)
 plt.title('FFT(FFTshift(echo)')
-plt.plot(np.abs(FFT_func_echolike_shifted))
-plt.plot(np.imag(FFT_func_echolike_shifted)); plt.legend(['real','imag'])
+plt.plot(torch.abs(FFT_func_echolike_shifted))
+plt.plot(torch.imag(FFT_func_echolike_shifted)); plt.legend(['real','imag'])
